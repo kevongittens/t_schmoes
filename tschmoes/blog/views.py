8a38273e262be7a1e0253955c571aaf9057fce7c
Kevon Gittens <kevon.gittens@yahoo.com>
2018-12-02 16:42:23 +0000

    pushing all files

--- tschmoes/blog/views.py
+++ tschmoes/blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Post
+'from django.http import HttpResponse'
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'blog/home.html')
+
+def about(request):
+
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/about.html', context)
