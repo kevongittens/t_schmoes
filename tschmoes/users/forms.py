859756a435ae8c31fe109d6b0fb06c66e8d58e17
Kevon Gittens <kevon.gittens@yahoo.com>
2018-12-07 05:27:19 +0000

    Changes to be committed: 	modified:   users/forms.py

--- tschmoes/users/forms.py
+++ tschmoes/users/forms.py
@@ -8,4 +8,4 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        field = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2']
