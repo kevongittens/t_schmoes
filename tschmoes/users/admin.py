2c23beaed7153b25683fe76d363ac39e46002a39
Kevon Gittens <kevon.gittens@yahoo.com>
2018-12-07 02:44:00 +0000

    heroku deploy changes 	modified:   users/views.py

--- tschmoes/users/admin.py
+++ tschmoes/users/admin.py
@@ -1,3 +1,3 @@
-from django.contrib import admin
+fromfromfromfrom djangofrom djdjangofrom django.contrib import admin
 
 # Register your models here.
--- tschmoes/users/views.py
+++ tschmoes/users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, 'Account created for %s' % username)
+            return redirect('blog-home')
+        else:
+            messages.success(request, 'Accont details not valid')
+    else:
+        form = UserRegisterForm()
+        messages.success(request, 'Accont details not valid')
+    return render(request, 'users/register.html', {'form': form})
